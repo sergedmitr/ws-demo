5aa24da8dc5f6732657e623619651163a894503c
sergdm <sergey.dmitr@mail.ru>
2023-06-21 08:08:20 +0300

    Add health endpoint

--- src/main/java/ru/sergdm/ws/ApiController.java
+++ src/main/java/ru/sergdm/ws/ApiController.java
@@ -1,13 +1,17 @@
 package ru.sergdm.ws;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
-	@GetMapping("/")
-	public String hello() {
-		return "Hello!";
+	@GetMapping("/health")
+	public ResponseEntity<Object> health() {
+		Health health = new Health("Ok");
+		return new ResponseEntity<>(health, HttpStatus.OK);
 	}
+	
 }
---
+++ src/main/java/ru/sergdm/ws/Health.java
@@ -0,0 +1,13 @@
+package ru.sergdm.ws;
+
+public class Health {
+	private String status;
+
+	public Health(String status) {
+		this.status = status;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+}
