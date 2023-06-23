ee13413f7e0b76f24936401722c203768623d46b
sergdm <sergey.dmitr@mail.ru>
2023-06-23 09:11:48 +0300

    Change message

--- src/main/java/ru/sergdm/ws/ApiController.java
+++ src/main/java/ru/sergdm/ws/ApiController.java
@@ -10,7 +10,7 @@ public class ApiController {
 
 	@GetMapping("/health")
 	public ResponseEntity<Object> health() {
-		Health health = new Health("Ok");
+		Health health = new Health("Ok world.");
 		return new ResponseEntity<>(health, HttpStatus.OK);
 	}
 	
