8c5b6689cbdfa092216ead276699bb4c7b7ea65b
sergdm <sergey.dmitr@mail.ru>
2023-10-15 09:48:48 +0300

    Add session toString

--- src/main/java/ru/sergdm/ws/model/Session.java
+++ src/main/java/ru/sergdm/ws/model/Session.java
@@ -3,4 +3,12 @@ package ru.sergdm.ws.model;
 public class Session {
 	public String sessionId;
 	public String userName;
+
+	@Override
+	public String toString() {
+		return "Session{" +
+				"sessionId='" + sessionId + '\'' +
+				", userName='" + userName + '\'' +
+				'}';
+	}
 }
